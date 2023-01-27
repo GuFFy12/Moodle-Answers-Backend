fdf30790138eefc0c4867e288594055184fc2432
GuFFy12 <golovin.mv.vl5@gmail.com>
2023-01-27 10:34:00 +0500

    Replace check to body in postAnswers.validator.ts

--- src/validators/postAnswers.validator.ts
+++ src/validators/postAnswers.validator.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
-import { check, validationResult } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 export default [
-	check('cmId', 'CM ID is required').exists().toInt().isInt({ min: 1 }),
-	check('percent', 'Percent is required').optional().isNumeric().isInt({ min: 0, max: 100 }),
-	check('answers', 'Answers is required').exists().isObject(),
+	body('cmId', 'CM ID is required').exists().toInt().isInt({ min: 1 }),
+	body('percent', 'Percent is required').optional().isNumeric().isInt({ min: 0, max: 100 }),
+	body('answers', 'Answers is required').exists().isObject(),
 	(req: Request, res: Response, next: NextFunction) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
